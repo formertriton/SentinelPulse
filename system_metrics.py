1a835fb1dc1adfb6b2527a4dda506f4d3e135d6d
Angelo R. <aromvos@gmail.com>
2025-08-04 13:00:14 -0700

    system_metrics.py

---
+++ system_metrics.py
@@ -0,0 +1,13 @@
+Build a system_metrics.py that returns:
+-CPU %
+-RAM used/total
+-Disk space
+-Uptime
+Run it manually and print the values every 5 seconds
+✅ Collected Metrics:
+-Hostname
+-OS version
+-CPU usage
+-Memory usage (absolute and %)
+-Disk usage
+-Uptime
